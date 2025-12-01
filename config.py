a8250410eab21fe66c41d44a10013affd8fa6693
WilsonNous <107373913+WilsonNous@users.noreply.github.com>
2025-12-01 18:29:42 -0300

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,11 @@ DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_NAME = os.getenv("DB_NAME")
 DB_HOST = os.getenv("DB_HOST")
+
 SECRET_KEY = os.getenv("SECRET_KEY", "brandoimoveis_secret")
+
+# 🔑 Senha do painel administrativo
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "@13Lorenzo")
 
 DB_PASS_ENCODED = quote_plus(DB_PASS) if DB_PASS else ""
 
@@ -17,4 +21,5 @@ DB_PASS_ENCODED = quote_plus(DB_PASS) if DB_PASS else ""
 SQLALCHEMY_DATABASE_URI = (
     f"mysql+mysqlconnector://{DB_USER}:{DB_PASS_ENCODED}@{DB_HOST}/{DB_NAME}?charset=utf8mb4"
 )
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
